9235c043332f1fd43e39a8b6a7043cb3c8e8fac5
bibinbobk <bibinbobk@hotmail.com>
2022-03-18 02:14:37 +0530

    Designed HomePage

---
+++ shosev/lib/maps.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class MyMap extends StatelessWidget {
+  const MyMap({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'lib/assets/img/Map.png',
+      fit: BoxFit.fitWidth,
+    );
+  }
+}
